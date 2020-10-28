c5f9286b5c9e75b215a282c6118eb0fe11efd69e
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-10-28 14:37:59 +0530

    fixes White screen appearing before splash screen #21

--- OnTimePro/app/src/main/java/com/example/ontimepro/MainActivity.java
+++ OnTimePro/app/src/main/java/com/example/ontimepro/MainActivity.java
@@ -12,40 +12,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
-
-    // variables
-    Animation topanim, bottomanim;
-    ImageView logo;
-    TextView title;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        // Animations
-
-       topanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
-       bottomanim= AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-
-        //Hooks
-        logo = findViewById(R.id.imageView);
-        title = findViewById(R.id.textView);
-
-        logo.setAnimation(topanim);
-        title.setAnimation(bottomanim);
-
-
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                 Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
-                 startActivity(intent);
-                 finish();
-
-            }
-        },SPLASH_SCREEN);
     }
 }
