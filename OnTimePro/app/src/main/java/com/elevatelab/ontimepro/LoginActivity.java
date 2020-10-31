ad50ca00a8fd1c1ca060e59b67f5bff0854a26c5
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-11-01 00:43:03 +0530

    Changed package name to com.elevatelab.ontimepro

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/LoginActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/LoginActivity.java
@@ -1,14 +1,14 @@
-package com.example.ontimepro;
+package com.elevatelab.ontimepro;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 }
