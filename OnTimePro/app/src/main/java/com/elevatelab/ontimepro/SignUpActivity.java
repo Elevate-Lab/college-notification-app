41900c5f6730313f621100955a4d8caa37223e1f
darsh8114 <73437932+darsh8114@users.noreply.github.com>
2020-11-03 22:57:13 +0530

    Move Sing up Activity to login Activity (#41)
    * Add files via upload
    
    * Add files via upload
    
    * Update SignUpActivity.java
    
    * Update SignUpActivity.java
    
    * Update SignUpActivity.java
    
    Co-authored-by: Siddhantdrk <siddhantdrk@gmail.com>

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/SignUpActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/SignUpActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private TextView sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        sign_in = (TextView) findViewById(R.id.signUp_txt);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
