f294d4c9464490ad096c5194a070e55c811f7881
Rahul Dev <iit2019053@iiita.ac.in>
2020-11-03 23:20:49 +0530

    intent updted

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/LoginActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/LoginActivity.java
@@ -2,18 +2,27 @@ package com.elevatelab.ontimepro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView mSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
-        startActivity(intent);
-        finish();
+        mSignUp=(TextView)findViewById(R.id.signUp_txt);
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
