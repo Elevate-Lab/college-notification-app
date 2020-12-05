2ac2ddc0bcd51437c64cc35c6e40a88caba34b39
Jyotika999 <IIT2019036@iiita.ac.in>
2020-12-05 14:22:47 +0530

    Added User class for authentication with respect Signup activity

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/models/User.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/models/User.java
@@ -1,4 +1,37 @@
 package com.elevatelab.ontimepro.models;
 
-public class User {
+public class User{
+
+    private String name;
+    private String mobile;
+    private String email;
+    private String password;
+
+
+    public User(String name, String mobile ,String email, String password) {
+        this.name = name;
+        this.mobile = mobile;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getMobile() {
+        return mobile;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
 }
