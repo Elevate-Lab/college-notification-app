10c29547a401d34be79d7cd9ca8e76b6f60f7cd8
Siddhantdrk <siddhantdrk@gmail.com>
2020-12-19 16:56:47 +0530

    Fixed #50 Validate and Check SignUp Fields.

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/Fragments/IndividualSignUpFragment.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/Fragments/IndividualSignUpFragment.java
@@ -9,12 +9,16 @@ import android.widget.Spinner;
 import androidx.fragment.app.Fragment;
 
 import com.elevatelab.ontimepro.R;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class IndividualSignUpFragment extends Fragment {
     private View rootView;
     private TextInputEditText nameEdt, emailEdt, passwordEdt, confirmPasswordEdt;
     private Spinner organizationSpinner;
+    private MaterialButton individualSignUpBtn;
+    private TextInputLayout nameTextInputLayout, emailTextInputLayout, passwordTextInputLayout, cnfPasswordTextInputLayout;
 
     public IndividualSignUpFragment() {
         // Required empty public constructor
@@ -26,7 +30,43 @@ public class IndividualSignUpFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_individual_sign_up, container, false);
         initViews();
+
+        individualSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateFields()) {
+
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    private boolean validateFields() {
+        boolean returnCheck = true;
+        if (nameEdt.getText().toString().isEmpty()) {
+            nameTextInputLayout.setError("Name is required !");
+            returnCheck = false;
+        }
+        if (emailEdt.getText().toString().isEmpty()) {
+            emailTextInputLayout.setError("Email is required !");
+            returnCheck = false;
+        }
+        if (passwordEdt.getText().toString().isEmpty()) {
+            passwordTextInputLayout.setError("Password is required !");
+            returnCheck = false;
+        }
+        if (confirmPasswordEdt.getText().toString().isEmpty()) {
+            cnfPasswordTextInputLayout.setError("Confirm Password is required !");
+            returnCheck = false;
+        }
+        if (!confirmPasswordEdt.getText().toString().equals(passwordEdt.getText().toString())) {
+            cnfPasswordTextInputLayout.setError("Confirm Password didn't match !");
+            returnCheck = false;
+        }
+
+        return returnCheck;
     }
 
     private void initViews() {
@@ -35,5 +75,10 @@ public class IndividualSignUpFragment extends Fragment {
         passwordEdt = rootView.findViewById(R.id.password_edit_text);
         confirmPasswordEdt = rootView.findViewById(R.id.confirm_password_edit_text);
         organizationSpinner = rootView.findViewById(R.id.registered_organization_spinner);
+        individualSignUpBtn = rootView.findViewById(R.id.individual_sign_up_btn);
+        nameTextInputLayout = rootView.findViewById(R.id.name_txt_input_layout);
+        emailTextInputLayout = rootView.findViewById(R.id.email_txt_input_layout);
+        passwordTextInputLayout = rootView.findViewById(R.id.password_text_input_layout);
+        cnfPasswordTextInputLayout = rootView.findViewById(R.id.cnf_password_text_input_layout);
     }
 }
--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/Fragments/OrganizationSignUpFragment.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/Fragments/OrganizationSignUpFragment.java
@@ -8,13 +8,17 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.elevatelab.ontimepro.R;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class OrganizationSignUpFragment extends Fragment {
 
     private View rootView;
     private TextInputEditText nameEdt, emailEdt, domainEdt, passwordEdt, confirmPasswordEdt;
+    private MaterialButton orgSignUpBtn;
+    private TextInputLayout nameTextInputLayout, emailTextInputLayout, domainTextInputLayout, passwordTextInputLayout, cnfPasswordTextInputLayout;
 
     public OrganizationSignUpFragment() {
         // Required empty public constructor
@@ -26,6 +30,16 @@ public class OrganizationSignUpFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_organization_signup, container, false);
         initViews();
+
+        orgSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateFields()) {
+
+                }
+            }
+        });
+
         return rootView;
     }
 
@@ -35,5 +49,41 @@ public class OrganizationSignUpFragment extends Fragment {
         domainEdt = rootView.findViewById(R.id.domain_edit_text);
         passwordEdt = rootView.findViewById(R.id.password_edit_text);
         confirmPasswordEdt = rootView.findViewById(R.id.confirm_password_edit_text);
+        orgSignUpBtn = rootView.findViewById(R.id.organization_sign_up_btn);
+        nameTextInputLayout = rootView.findViewById(R.id.name_txt_input_layout);
+        emailTextInputLayout = rootView.findViewById(R.id.email_txt_input_layout);
+        domainTextInputLayout = rootView.findViewById(R.id.domain_text_input_layout);
+        passwordTextInputLayout = rootView.findViewById(R.id.password_text_input_layout);
+        cnfPasswordTextInputLayout = rootView.findViewById(R.id.cnf_text_input_layout);
+    }
+
+    private boolean validateFields() {
+        boolean returnCheck = true;
+        if (nameEdt.getText().toString().isEmpty()) {
+            nameTextInputLayout.setError("Name is required !");
+            returnCheck = false;
+        }
+        if (emailEdt.getText().toString().isEmpty()) {
+            emailTextInputLayout.setError("Email is required !");
+            returnCheck = false;
+        }
+        if (domainEdt.getText().toString().isEmpty()) {
+            domainTextInputLayout.setError("Domain is required !");
+            returnCheck = false;
+        }
+        if (passwordEdt.getText().toString().isEmpty()) {
+            passwordTextInputLayout.setError("Password is required !");
+            returnCheck = false;
+        }
+        if (confirmPasswordEdt.getText().toString().isEmpty()) {
+            cnfPasswordTextInputLayout.setError("Confirm Password is required !");
+            returnCheck = false;
+        }
+        if (!confirmPasswordEdt.getText().toString().equals(passwordEdt.getText().toString())) {
+            cnfPasswordTextInputLayout.setError("Confirm Password didn't match !");
+            returnCheck = false;
+        }
+
+        return returnCheck;
     }
 }
