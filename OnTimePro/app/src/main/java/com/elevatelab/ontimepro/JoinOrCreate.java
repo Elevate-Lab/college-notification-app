d8af2d725d0b32261d71f00b2b7bfd0a4d918c8b
Jyotika999 <IIT2019036@iiita.ac.in>
2020-12-22 14:54:36 +0530

    Set up the widgets and design for JoinOrCreate activity

---
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/JoinOrCreate.java
@@ -0,0 +1,14 @@
+package com.elevatelab.ontimepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinOrCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_or_create);
+    }
+}
