1db764928f3fd9e7e42c35ec47c8ce45562a3f46
Pranhu-Tripathi <mtpranshu2001@gmail.com>
2020-12-30 16:19:57 +0530

    Main Activity Recieves Institution Code

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/WelcomeActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/WelcomeActivity.java
@@ -2,35 +2,41 @@ package com.elevatelab.ontimepro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class WelcomeActivity extends AppCompatActivity {
 
 
     private Button registerBtn, loginBtn;
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseFirestore db;
+    private String instiIdOfCurrent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         initViews();
         mFirebaseAuth = FirebaseAuth.getInstance();
-
+        db = FirebaseFirestore.getInstance();
         if (mFirebaseAuth.getCurrentUser() != null) {
-            Intent mainActivityIntent = new Intent(WelcomeActivity.this, MainActivity.class);
-            startActivity(mainActivityIntent);
-            finish();
+
+            String userID = mFirebaseAuth.getUid();
+            startIntent(userID);
         }
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signUpIntent = new Intent(WelcomeActivity.this, com.elevatelab.ontimepro.SignUpActivity.class);
+                Intent signUpIntent = new Intent(WelcomeActivity.this, SignUpActivity.class);
                 startActivity(signUpIntent);
                 finish();
             }
@@ -39,7 +45,7 @@ public class WelcomeActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(WelcomeActivity.this, com.elevatelab.ontimepro.LoginActivity.class);
+                Intent loginIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
                 startActivity(loginIntent);
                 finish();
             }
@@ -49,4 +55,24 @@ public class WelcomeActivity extends AppCompatActivity {
         registerBtn = findViewById(R.id.login_register_btn);
         loginBtn = findViewById(R.id.login_btn);
     }
+
+    private void startIntent(String userID)
+    {
+        db.collection("Users")
+                .document(userID)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        instiIdOfCurrent = (String) documentSnapshot.get("instiID");
+                        Log.e("INs",instiIdOfCurrent);
+                        Intent mainActivityIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                        mainActivityIntent.putExtra("instiCode",instiIdOfCurrent);
+                        startActivity(mainActivityIntent);
+                        finish();
+                    }
+                });
+
+    }
+
 }
