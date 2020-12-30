f5686d3ab27a6b8fff802379f7ec79821f161ddc
Jyotika <54600270+Jyotika999@users.noreply.github.com>
2020-12-30 23:16:05 +0530

    Added Join Channel activity design and widgets (#75)

---
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/Join_channel.java
@@ -0,0 +1,14 @@
+package com.elevatelab.ontimepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Join_channel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_channel);
+    }
+}
