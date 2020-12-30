0b5dd9c3ba2c2c1e0bc9d387c312a0a7587cf4df
Pranshu Tripathi <mtpranshu2001@gmail.com>
2020-12-30 16:43:44 +0530

    Authenticate Channel Creation (#74)

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/JoinOrCreateActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/JoinOrCreateActivity.java
@@ -4,28 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class JoinOrCreateActivity extends AppCompatActivity {
 
 
     private Button joinChannelOption;
     private Button createChannelOption;
+    private Intent receiver;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_or_create);
 
+        receiver = getIntent();
+        final String instiCode = receiver.getStringExtra("instiCode");
         joinChannelOption = findViewById(R.id.joinChannelOption);
         createChannelOption = findViewById(R.id.createChannelOption);
-
+        auth = FirebaseAuth.getInstance();
         createChannelOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(JoinOrCreateActivity.this,CreateChannelActivity.class);
-                startActivity(intent);
+                if(auth.getUid().equals(instiCode))
+                {
+                    Intent intent = new Intent(JoinOrCreateActivity.this,CreateChannelActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(JoinOrCreateActivity.this, "Only For Admins", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
