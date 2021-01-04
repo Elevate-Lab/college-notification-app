cbf771c1092e21dd6fce32e5523185c941ecaa77
Pranshu Tripathi <mtpranshu2001@gmail.com>
2021-01-05 02:27:07 +0530

    Auto Code Generator Added (#81)

--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/BackgroundServices/Services.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/BackgroundServices/Services.java
@@ -1,10 +1,55 @@
 package com.elevatelab.ontimepro.BackgroundServices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Services {
 
     public static boolean mailIdValidation(String emailId)
     {
         return emailId.endsWith("@iiita.ac.in");
+    }
+
+    public static String codeGenerator(ArrayList<String> inUseCodes)
+    {
+        String generatedCode = "";
+        List<String> alphaNumeric = new ArrayList<>();
+        alphaNumeric = Arrays.asList(
+                "1", "2","3","4","5","6","7","8","9","0",
+                "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+                "w","x","y","z",
+                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V",
+                "W","X","Y","Z"
+        );
+
+        for(int i = 0 ; i < 6 ; i++)
+        {
+            generatedCode += alphaNumeric.get(i);
+        }
+
+        while(isDuplicateCode(inUseCodes,generatedCode))
+        {
+            generatedCode = "";
+            Collections.shuffle(alphaNumeric);
+            for(int i = 0 ; i < 6 ; i++)
+            {
+                generatedCode += alphaNumeric.get(i);
+            }
+        }
+
+        return generatedCode;
+    }
+
+    private static boolean isDuplicateCode(ArrayList<String> inUseCodes,String generatedCode)
+    {
+        for(String code : inUseCodes)
+        {
+            if(code.equals(generatedCode))
+                return true;
+        }
+        return false;
     }
 
 
--- OnTimePro/app/src/main/java/com/elevatelab/ontimepro/CreateChannelActivity.java
+++ OnTimePro/app/src/main/java/com/elevatelab/ontimepro/CreateChannelActivity.java
@@ -1,14 +1,51 @@
 package com.elevatelab.ontimepro;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.elevatelab.ontimepro.BackgroundServices.Services;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreateChannelActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_channel);
+        db = FirebaseFirestore.getInstance();
+        retrieveCode();
+
+    }
+
+    private void retrieveCode()
+    {
+        db.collection("developers")
+                .document("codesGenerated")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        ArrayList<String> inUseCodes = (ArrayList<String>) documentSnapshot.get("codes");
+
+                        String newChannelCode = Services.codeGenerator(inUseCodes);
+
+                        Toast.makeText(CreateChannelActivity.this, newChannelCode, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
